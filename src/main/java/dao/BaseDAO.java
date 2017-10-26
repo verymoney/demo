1bdc05ef02bc05e03a53d7df795919e13efdaa8d
verymoney <2655387212@qq.com>
2017-10-26 19:24:23 +0800

    fdsaf

---
+++ src/main/java/dao/BaseDAO.java
@@ -0,0 +1,7 @@
+package dao;
+
+/**
+ * Created by Administrator on 2017/10/20.
+ */
+public class BaseDAO {
+}
---
+++ src/main/java/dao/DeptDAO.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * Created by Administrator on 2017/10/20.
+ */
+public class DeptDAO extends ActionSupport {
+
+}
